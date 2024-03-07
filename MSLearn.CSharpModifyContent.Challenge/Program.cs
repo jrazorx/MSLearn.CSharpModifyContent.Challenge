d50532c4937dc88a507399cff19f2563e271549e
jra <julien.gault.50@gmail.com>
2024-03-07 17:22:05 +0100

    Add project files.

---
+++ MSLearn.CSharpModifyContent.Challenge/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Begin adding your solution code to the starter code under the comment // Your work here.
+
+Set the quantity variable to the value obtained by extracting the text between the <span> and </span> tags.
+
+Set the output variable to the value of input, then remove the <div> and </div> tags.
+
+Replace the HTML character ™ (&trade;) with ® (&reg;) in the output variable.
+
+Run your solution and verify the output put matches the expected output.
+Quantity: 5000
+Output: <h2>Widgets &reg;</h2><span>5000</span>
+*/
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
