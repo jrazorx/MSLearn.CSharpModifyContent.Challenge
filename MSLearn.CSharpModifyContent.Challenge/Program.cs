c28a75752fe7a28eddb254f59da204f6b642af86
jra <julien.gault.50@gmail.com>
2024-03-07 17:45:33 +0100

    Set the quantity variable to the value obtained by extracting the text between the <span> and </span> tags.

--- MSLearn.CSharpModifyContent.Challenge/Program.cs
+++ MSLearn.CSharpModifyContent.Challenge/Program.cs
@@ -18,6 +18,31 @@ string quantity = "";
 string output = "";
 
 // Your work here
+string[] divTag         = { "<div>" , "</div>"  };
+string[] h2Tag          = { "<h2>"  , "</h2>"   };
+string[] spanTag        = { "<span>", "</span>" };
+string[] currentTag     = new string[2];
+int[] currentTagLengths = new int[2];
+
+int openingTagIndex = 0;
+int closingTagIndex = 0;
+
+// Set the quantity variable to the value obtained by extracting the text between the <span> and </span> tags.
+// Initialization
+currentTag = spanTag;
+for (int i = 0; i < currentTag.Length; i++)
+{
+    currentTagLengths[i] = currentTag[i].Length;
+}
+
+openingTagIndex = input.IndexOf(currentTag[0]);
+if (openingTagIndex != -1)
+{
+    closingTagIndex = input.IndexOf(currentTag[1]);
+    if (closingTagIndex != -1)
+        quantity = "Quantity: " + input.Substring(openingTagIndex + currentTagLengths[0], closingTagIndex - (openingTagIndex + currentTagLengths[0]));
+}
+
 
 Console.WriteLine(quantity);
 Console.WriteLine(output);
